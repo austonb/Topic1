ab1bc34f4888c742e6794fdad885a0506a912bd8
austonb <p2025042@mvrht.com>
2017-05-15 13:41:41 -0700

    Add files via upload

---
+++ Exercise1_8.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Exercise1_8 {
+	public static class Ex01_08 {
+		 
+		 public static void main(String[] args) {
+		 
+		  System.out.println("Radius of circle = 5.5");
+		  System.out.println("Perimeter = 2 x radius x pi = " + 2 * 5.5 * Math.PI);
+		  System.out.println("Area = radius x radius x pi = " + 5.5 * 5.5
+		    * Math.PI);
+		 }
+}}
