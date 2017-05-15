bc91e474e1cd91d47164d959a7129f0e284859dd
austonb <p2025042@mvrht.com>
2017-05-15 13:45:51 -0700

    Add files via upload

---
+++ Exercise1_10.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Exercise1_10 {
+	 public static void main(String[] strings) {
+
+	        double kilometers = 14.0;
+	        double miles = kilometers / 1.6;
+
+	        double rate = (45.5 * 60.0 + 30.0) / (60.0 * 60.0);
+	        double milesPerHour = miles / rate;
+
+	        System.out.println(milesPerHour);
+
+
+	    }
+
+}
