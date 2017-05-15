a27b2cd4462187d384fc447d55bf362f35ae1a92
austonb <p2025042@mvrht.com>
2017-05-15 13:17:54 -0700

    Add files via upload

---
+++ Exercise1_7.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Exercise1_7 {
+	
+	    public static void main(String[] args)
+	    {
+	        System.out.print("This will print 1st approximation: ");
+	        System.out.println(4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)-(1.0/11)));
+	        System.out.print("This will print 2nd approximation: ");
+	        System.out.println(4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)-(1.0/11)+(1.0/13)));
+	    }
+}
